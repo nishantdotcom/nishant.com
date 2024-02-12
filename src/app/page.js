3ab9eb3952a5857e62eb21f6a4b600240015e87e
nishantkumar1308 <nishantsingh1308@gmail.com>
2024-02-12 22:22:31 +0530

    update in navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,27 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <>
-      <h1>My portfolio</h1>
+      <div className=" flex justify-between bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 ">
+        <div className=" font-monow-1/2   p-6  ml-10px"> NK</div>
+        <div className="  font-mono w-1/2  p-6   flex justify-around gap-x-10">
+          <div className=" flex">
+            <div className="font-semibold px-2 text-black">01. </div>
+            <div className="font-semibold"> Skills</div>
+          </div>
+          <div className=" flex">
+            <div className="font-semibold px-2 text-black">02. </div>
+            <div className="font-semibold"> Projects</div>
+          </div>
+          <div className=" flex">
+            <div className="font-semibold px-2 text-black">03. </div>
+            <div className="font-semibold"> Experience</div>
+          </div>
+          <div className=" flex">
+            <div className="font-semibold px-2 text-black">04. </div>
+            <div className="font-semibold"> Contact</div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
