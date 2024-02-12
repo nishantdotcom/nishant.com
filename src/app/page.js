d223752a19e2f7c4856587dea61c8002644b066a
nishantkumar1308 <nishantsingh1308@gmail.com>
2024-02-12 22:50:37 +0530

    Navbar update

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 export default function Home() {
   return (
     <>
-      <div className=" flex justify-between bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 ">
-        <div className=" font-monow-1/2   p-6  ml-10px"> NK</div>
+      <div className=" flex justify-between bg-darkNavy  shadow-2xl bg-opacity-60 backdrop-filter: blur(20px);">
+        <div className=" font-mono w-1/2   font-bold text-xl text-[#2d3748] p-6  ml-10px">
+          NK
+        </div>
         <div className="  font-mono w-1/2  p-6   flex justify-around gap-x-10">
           <div className=" flex">
             <div className="font-semibold px-2 text-black">01. </div>
-            <div className="font-semibold"> Skills</div>
+            <div className="font-semibold hover:text-red-400"> Skills</div>
           </div>
           <div className=" flex">
             <div className="font-semibold px-2 text-black">02. </div>
