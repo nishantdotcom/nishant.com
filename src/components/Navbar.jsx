241bd87f8cc064778e503f757f57704dd79ad6b2
nishantkumar1308 <nishantsingh1308@gmail.com>
2024-02-13 15:11:59 +0530

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,6 +14,11 @@ function Navbar() {
     { id: "04.", specs: "Contact" },
   ];
 
+  const variants = {
+    visible: { opacity: 1 },
+    hidden: { opacity: 0 },
+  };
+
   const handleClick = () => {
     if (isSidebar == 1) {
       setisSidebar(0);
@@ -32,20 +37,20 @@ function Navbar() {
         animate={{ opacity: 1 }}
         transition={{ duration: 0.9, ease: "easeInOut" }}
       >
-        <div className=" flex shadow-2xl w-auto">
-          <div className="font-mono w-1/5  flex justify-start  font-bold text-3xl text-[#2d3748] p-6  ml-10px">
+        <div className=" bg-[#0A192F] flex shadow-2xl w-auto">
+          <div className="font-mono w-1/5   flex justify-start  font-bold text-3xl text-[#22D3EE] p-6  ml-10px">
             NK
           </div>
           <div className=" hidden  md:flex w-4/5 justify-end px-[4vw] ">
             <div className="   flex font-mono p-6 gap-x-10">
               {details.map((data, index) => (
                 <>
-                  <div className=" flex" key={index}>
-                    <div className="font-semibold px-2 text-black" key={index}>
+                  <div className=" flex mt-2" key={index}>
+                    <div className=" text-sm px-2 text-[#22D3EE]" key={index}>
                       {data.id}
                     </div>
                     <div
-                      className="font-semibold hover:text-red-400"
+                      className=" text-white hover:text-[#22D3EE] hover:cursor-pointer text-sm"
                       key={index}
                     >
                       {data.specs}
@@ -67,7 +72,7 @@ function Navbar() {
               width="40"
               height="40"
               viewBox="0 0 32 32"
-              className=""
+              className=" text-white"
             >
               <path d="M 4 7 L 4 9 L 28 9 L 28 7 L 4 7 z M 4 15 L 4 17 L 28 17 L 28 15 L 4 15 z M 4 23 L 4 25 L 28 25 L 28 23 L 4 23 z"></path>
             </svg>
@@ -78,22 +83,27 @@ function Navbar() {
             <div></div>
           </>
         ) : (
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 0.9, ease: "easeInOut" }}
-          >
+          <motion.div initial="hidden" animate="visible" variants={variants}>
             <div class=" flex h-screen justify-end shadow-2xl shadow-indigo-500/50">
-              <div class="w-40 bg-gray-100  p-2">
+              <div class="w-40 bg-[#0A192F]  p-2">
                 <ul>
                   {details.map((data, index) => (
                     <>
-                      <li className="mb-2" key={index}>
-                        <div className=" flex">
-                          <div className="font-semibold px-2 text-black">
+                      <li
+                        className="mb-2 justify-around pt-8 hover:cursor-pointer"
+                        key={index}
+                      >
+                        <div className=" flex ">
+                          <div
+                            className="text-sm px-2 text-[#22D3EE]"
+                            key={index}
+                          >
                             {data.id}
                           </div>
-                          <div className="font-semibold hover:text-red-400">
+                          <div
+                            className="text-sm px-2 text-white hover:text-[#22D3EE]"
+                            key={index}
+                          >
                             {data.specs}
                           </div>
                         </div>
